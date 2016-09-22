6867618fd72594041d2b07177d9836691997e518
Maksym Gerashchenko <maksym_gerashchenko@epam.com>
2016-09-22 13:17:23 +0200

    init2

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: '<h1>My First Angular App</h1>'
 })
-export class AppComponent { }
+class AppComponent { }
+
+export default AppComponent
